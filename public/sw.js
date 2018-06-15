3707f055ee964ddbb8da1cea5f71990133afd39a
Basavaraj K N <Basavaraj.K@Stackroute.in>
2018-06-15 15:56:35 +0530

    upgraded to workbox3

---
+++ public/sw.js
@@ -0,0 +1,36 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox-sw.js");
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [
+  {
+    "url": "favicon.ico",
+    "revision": "d1c582193c63639a5ab4d2c02751492e"
+  },
+  {
+    "url": "index.html",
+    "revision": "9914ff467df4676f5ff2c93da0cf532c"
+  },
+  {
+    "url": "manifest.json",
+    "revision": "730915ec2725cf6721080a6516b23f15"
+  }
+].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
---
+++ workbox-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  "globDirectory": "build/",
+  "globPatterns": [
+    "**/*.{json,ico,html,js,css}"
+  ],
+  "swDest": "build/sw.js"
+};
